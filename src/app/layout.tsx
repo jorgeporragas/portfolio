b7b228f33f202638fb92c14dcd7d328b5e75f884
jorgeporragas <jorgeporragas@gmail.com>
2026-03-19 23:42:04 -0600

    chore: update project metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "jorge porragas | portafolio",
+  title: "jorge porragas | portfolio",
   description: "software developer & producer",
 };
 
